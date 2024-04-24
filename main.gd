c4602177aeaada8a893240c1d7ed85f54ea42051
arihkm <airilhakimi2005@gmail.com>
2024-04-24 13:46:45 +0800

    Minor changes

--- main.gd
+++ main.gd
@@ -6,3 +6,7 @@ func _on_quit_button_2_pressed():
 	get_tree().quit()
 func _on_start_button_2_pressed():
 	get_tree().change_scene_to_file("res://Scene1.tscn")
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://NewGame.tscn")
