6551ea477da6c60265675372cdf2a084c0ae9d60
arihkm <airilhakimi2005@gmail.com>
2024-04-24 23:19:49 +0800

    changed musics, added shop scene, added music for that too, and uhh, some other minor changes i think

--- main.gd
+++ main.gd
@@ -6,7 +6,7 @@ func _on_quit_button_2_pressed():
 	get_tree().quit()
 func _on_start_button_2_pressed():
 	get_tree().change_scene_to_file("res://Scene1.tscn")
-
-
-func _on_button_pressed():
+func _on_shop_button_pressed():
+	get_tree().change_scene_to_file("res://Shop.tscn")
+func _on_new_game_button_2_pressed():
 	get_tree().change_scene_to_file("res://NewGame.tscn")
