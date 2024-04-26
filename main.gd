08774c47e168016618c0df8424b88ed219a58f5f
arihkm <airilhakimi2005@gmail.com>
2024-04-26 09:47:48 +0800

    minorchange

--- main.gd
+++ main.gd
@@ -8,3 +8,9 @@ func _on_start_button_2_pressed():
 	get_tree().change_scene_to_file("res://Scene1.tscn")
 func _on_shop_button_pressed():
 	get_tree().change_scene_to_file("res://Shop.tscn")
+
+
+
+
+func _on_test_button_pressed():
+	get_tree().change_scene_to_file("res://teest")
