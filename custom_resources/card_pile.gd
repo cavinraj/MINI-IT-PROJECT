72a28bf3247b0d8a2242451fa96b0995cf9bee2e
Raajshreedhar <raaj.shreedhar@gmail.com>
2024-04-28 13:32:29 +0800

    Added Card Pile Functions

--- custom_resources/card_pile.gd
+++ custom_resources/card_pile.gd
@@ -34,4 +34,4 @@ func _to_string() -> String:
 	var _card_strings: PackedStringArray = []
 	for i in range(cards.size()):
 		_card_strings.append("%s: %s" % [i+1, cards[i].id])
-	return "\n".join(_card_strings)
+	return "\n".join(_card_strings) 
