75a88f7c45da584328ef47f9f3cff5fe2ead9dbc
Cavin Raj <cavinraj96@gmail.com>
2024-05-01 22:58:34 +0800

    fixing error

---
+++ custom_resources/card.gd
@@ -0,0 +1,13 @@
+extends Resource
+class_name Card
+
+enum Type {ATTACK, SKILL, POWER}
+enum Target {SELF, SINGLE_ENEMY, ALL_ENEMIES, EVERYONE}
+@export_group ("Card Attributes")
+@export var id: String
+@export var type: Type
+@export var target: Target
+
+
+func is_single_targeted() -> bool:
+	return target == Target.SINGLE_ENEMY
---
+++ main.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func _on_quit_button_2_pressed():
+	get_tree().quit()
+func _on_start_button_2_pressed():
+	get_tree().change_scene_to_file("res://Scene1.tscn")
+func _on_shop_button_pressed():
+	get_tree().change_scene_to_file("res://Shop.tscn")
