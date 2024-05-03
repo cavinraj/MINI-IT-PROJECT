f6f39309421d0a8c3e32be81f926ab8ebdd608e8
Raajshreedhar <raaj.shreedhar@gmail.com>
2024-05-03 20:35:28 +0800

    Visual feedback for unplayable cards (when you have not enough mana)

--- Scenes (CardUI)/card_ui/card_ui.gd
+++ Scenes (CardUI)/card_ui/card_ui.gd
@@ -8,7 +8,7 @@ const DRAG_STYLEBOX := preload("res://Scenes (CardUI)/card_ui/card_dragging_styl
 const HOVER_STYLEBOX := preload("res://Scenes (CardUI)/card_ui/card_hover_stylebox.tres")
 
 @export var card: Card : set = _set_card
-@export var char_stats: CharacterStats
+@export var char_stats: CharacterStats : set = _set_char_stats
 
 @onready var panel: Panel = $Panel
 @onready var cost: Label = $Cost
@@ -19,9 +19,15 @@ const HOVER_STYLEBOX := preload("res://Scenes (CardUI)/card_ui/card_hover_styleb
 
 var parent: Control #the card parent state
 var tween: Tween
+var playable := true : set = _set_playable
+var disabled := false
 
 
 func _ready():
+	Events.card_aim_started.connect(_on_card_drag_or_aiming_started)
+	Events.card_drag_started.connect(_on_card_drag_or_aiming_started)
+	Events.card_aim_ended.connect(_on_card_drag_or_aim_ended)
+	Events.card_drag_ended.connect(_on_card_drag_or_aim_ended)
 	card_state_machine.init(self)
 	
 	
@@ -59,6 +65,21 @@ func _set_card(value: Card) -> void:
 	icon.texture = card.icon
 
 
+func _set_playable(value: bool) -> void:
+	playable = value
+	if not playable:
+		cost.add_theme_color_override("font_color", Color.RED)
+		icon.modulate = Color(1,1,1,0.5)
+	else:
+		cost.remove_theme_color_override("font_color")
+		icon.modulate = Color(1,1,1,1)
+
+
+func _set_char_stats(value: CharacterStats) -> void:
+	char_stats = value
+	char_stats.stats_changed.connect(_on_char_stats_changed)
+
+
 func _on_drop_point_detector_area_entered(area: Area2D) -> void:
 	if not targets.has(area):
 		targets.append(area)
@@ -66,3 +87,19 @@ func _on_drop_point_detector_area_entered(area: Area2D) -> void:
 
 func _on_drop_point_detector_area_exited(area: Area2D) -> void:
 	targets.erase(area)
+
+
+func _on_card_drag_or_aiming_started(used_card: CardUI) -> void:
+	if used_card == self:
+		return
+	
+	disabled = true
+
+
+func _on_card_drag_or_aim_ended(_card: CardUI) -> void:
+	disabled = false
+	self.playable = char_stats.can_play_card(card)
+
+
+func _on_char_stats_changed() -> void:
+	self.playable = char_stats.can_play_card(card)
