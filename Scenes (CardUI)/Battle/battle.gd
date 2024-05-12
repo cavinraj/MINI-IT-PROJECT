6adbf49e13a0df95d2e56f19512fe93ef6811ca6
Raajshreedhar <raaj.shreedhar@gmail.com>
2024-05-13 01:10:40 +0800

    Win/Lose screen added to the game

--- Scenes (CardUI)/Battle/battle.gd
+++ Scenes (CardUI)/Battle/battle.gd
@@ -28,6 +28,7 @@ func _ready() -> void:
 	
 	
 func start_battle(stats: CharacterStats) -> void:
+	get_tree().paused = false
 	MusicPlayer.play(music, true)
 	enemy_handler.reset_enemy_actions()
 	player_handler.start_battle(stats)
@@ -35,7 +36,7 @@ func start_battle(stats: CharacterStats) -> void:
 
 func _on_enemies_child_order_changed() -> void:
 	if enemy_handler.get_child_count() == 0:
-		print("Victory!")
+		Events.battle_over_screen_requested.emit("Victorious! :)", BattleOverPanel.Type.WIN )
 		
 	
 func _on_enemy_turn_ended() -> void:
@@ -44,4 +45,4 @@ func _on_enemy_turn_ended() -> void:
 
 
 func _on_player_died() -> void:
-	print("Game Over!")
+	Events.battle_over_screen_requested.emit("Game Over! :(", BattleOverPanel.Type.LOSE)
