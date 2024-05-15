c3eabeb5d6d7bfea179d00697d76027a4ac22e34
Cavin Raj <cavinraj96@gmail.com>
2024-05-15 22:51:56 +0800

    Polished Main Menu

--- Scenes (CardUI)/ui/main_menu.gd
+++ Scenes (CardUI)/ui/main_menu.gd
@@ -1,1 +1,20 @@
 extends Control
+
+@onready var continue_button: Button = %Continue
+
+
+func _ready() -> void:
+	get_tree().paused = false
+	
+
+func _on_continue_pressed() -> void:
+	print("continue run")
+
+
+func _on_new_run_pressed() -> void:
+	print("new run")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+	
