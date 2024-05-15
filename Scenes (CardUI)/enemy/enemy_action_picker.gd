3484d370d169c9f8ca422364e8d269cb37505ec5
Cavin Raj <cavinraj96@gmail.com>
2024-05-15 22:34:06 +0800

    Visuals for the Main Menu Scene is complete

--- Scenes (CardUI)/enemy/enemy_action_picker.gd
+++ Scenes (CardUI)/enemy/enemy_action_picker.gd
@@ -32,11 +32,9 @@ func get_first_conditional_action() -> EnemyAction:
 	
 	
 func get_chance_based_action() -> EnemyAction:
-	var action: EnemyAction
 	var roll := randf_range(0.0, total_weight)
 	
-	for child in get_children():
-		action = child as EnemyAction
+	for action: EnemyAction in get_children():
 		if not action or action.type != EnemyAction.Type.CHANCE_BASED:
 			continue
 			
