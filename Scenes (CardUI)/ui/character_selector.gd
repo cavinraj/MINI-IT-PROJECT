badced329e5e8a5895ef42487288160299db79f3
Cavin Raj <cavinraj96@gmail.com>
2024-05-16 00:03:28 +0800

    Starting the script for Character Selector

---
+++ Scenes (CardUI)/ui/character_selector.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_warrior_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_wizard_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_assassin_button_pressed():
+	pass # Replace with function body.
