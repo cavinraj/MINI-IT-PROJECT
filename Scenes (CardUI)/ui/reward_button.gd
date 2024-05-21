66ba6bdf2d27c31e19b2438aca626f72f21571b8
Cavin Raj <cavinraj96@gmail.com>
2024-05-21 22:15:25 +0800

    Started The Script For Reward Button

---
+++ Scenes (CardUI)/ui/reward_button.gd
@@ -0,0 +1,21 @@
+class_name RewardButton
+extends Button
+
+@export var reward_icon: Texture : set = set_reward_icon
+@export var reward_text: String : set = set_reward_text
+
+@onready var custom_icon: TextureRect = %CustomIcon
+@onready var custom_text: Label = %CustomText
+
+
+func set_reward_icon(new_icon: Texture) -> void:
+	reward_icon = new_icon
+	
+	if not is_node_ready():
+		await ready
+		
+	custom_icon.texture = reward_icon
+	
+	
+func _on_pressed():
+
