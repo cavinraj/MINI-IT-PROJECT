2efcd4ca28e15be12cfd545152baf90b67c9bab7
Cavin Raj <cavinraj96@gmail.com>
2024-05-21 22:19:30 +0800

    Completed The Reward Button Script

--- Scenes (CardUI)/ui/reward_button.gd
+++ Scenes (CardUI)/ui/reward_button.gd
@@ -17,5 +17,15 @@ func set_reward_icon(new_icon: Texture) -> void:
 	custom_icon.texture = reward_icon
 	
 	
+func set_reward_text(new_text: String) -> void:
+	reward_text = new_text
+	
+	if not is_node_ready():
+		await ready
+		
+	custom_text.text = reward_text
+	
+	
 func _on_pressed():
+	queue_free()
 
