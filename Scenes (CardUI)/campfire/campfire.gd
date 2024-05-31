f9aa44f67977c61f91121e240eb7bd7550f9d9f0
Raajshreedhar <raaj.shreedhar@gmail.com>
2024-06-01 01:47:11 +0800

    Added Campfire scripting

--- Scenes (CardUI)/campfire/campfire.gd
+++ Scenes (CardUI)/campfire/campfire.gd
@@ -1,5 +1,17 @@
+class_name Campfire
 extends Control
 
+@export var char_stats: CharacterStats
 
-func _on_button_pressed():
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _on_rest_button_pressed() -> void:
+	char_stats.heal(ceili(char_stats.max_health * 0.3))
+	animation_player.play("fade_out")
+
+
+# This is called from the Animation Player
+#at the end of the 'fade_out'.
+func _on_fade_out_finished() -> void:
 	Events.campfire_exited.emit()
