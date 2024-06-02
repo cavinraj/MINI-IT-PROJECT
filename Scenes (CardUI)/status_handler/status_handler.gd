999724c401681325fef7c0210f451902c647b91c
Cavin Raj <cavinraj96@gmail.com>
2024-06-03 00:08:42 +0800

    Created StatusHandler Script

--- Scenes (CardUI)/status_handler/status_handler.gd
+++ Scenes (CardUI)/status_handler/status_handler.gd
@@ -1,1 +1,62 @@
+class_name  StatusHandler
 extends GridContainer
+
+const  STATUS_UI = preload("res://Scenes (CardUI)/status_handler/status_ui.tscn")
+
+@export var status_owner: Node2D
+
+
+func add_status(status: Status) -> void:
+	var stackable := status.stack_type != Status.StackType.NONE
+	
+	# Add it if it's new
+	if not _has_status(status.id):
+		var new_status_ui := STATUS_UI.instantiate() as StatusUI
+		add_child(new_status_ui)
+		new_status_ui.status = status
+		new_status_ui.status.status_applied.connect(_on_status_applied)
+		new_status_ui.status.initialize_status(status_owner)
+		return
+		
+	# If it's unique and we already have it, we can return
+	if not status.can_expire and not stackable:
+		return
+		
+	# If it's duration-stackable, expand it
+	if status.can_expire and status.stack_type == Status.StackType.DURATION:
+		_get_status(status.id).duration += status.duration
+		return
+		
+	# If it's stackable, stack it
+	if status.stack_type == Status.StackType.INTENSITY:
+		_get_status(status.id).stacks += status.stacks
+		
+		
+func _has_status(id: String) -> bool:
+	for status_ui: StatusUI in get_children():
+		if status_ui.status.id == id:
+			return true
+			
+	return false
+	
+	
+func _get_status(id: String) -> Status:
+	for status_ui: StatusUI in get_children():
+		if status_ui.status.id == id:
+			return status_ui.status
+			
+	return null
+	
+	
+func _get_all_statuses() -> Array[Status]:
+	var statuses: Array[Status] = []
+	for status_ui: StatusUI in get_children():
+		statuses.append(status_ui.status)
+		
+	return statuses
+	
+	
+func _on_status_applied(status: Status) -> void:
+	if status.can_expire:
+		status.duration -= 1
+		
