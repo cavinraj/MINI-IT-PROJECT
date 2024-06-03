5be8382105c9177b05b8ef27b7e43d2aa106f5d3
arihkm <airilhakimi2005@gmail.com>
2024-06-03 20:55:39 +0800

    finishedmodifiervaluescript

--- Scenes (CardUI)/modifier_handler/modifier_value.gd
+++ Scenes (CardUI)/modifier_handler/modifier_value.gd
@@ -7,3 +7,11 @@ enum Type {PERCENT_BASED, FLAT}
 @export var percent_value: float
 @export var flat_value: int
 @export var source: String
+
+
+static func create_new_modifier(modifier_source: String, what_type: Type) -> ModifierValue:
+	var new_modifier := new()
+	new_modifier.source = modifier_source
+	new_modifier.type = what_type
+	
+	return new_modifier
