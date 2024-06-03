98ecf5de45653d10290fdfe93fa843c6343e38c2
arihkm <airilhakimi2005@gmail.com>
2024-06-03 22:02:34 +0800

    modified muscle.gd to include all the new modifier functions

--- Scenes (CardUI)/player/player.gd
+++ Scenes (CardUI)/player/player.gd
@@ -6,6 +6,7 @@ extends Node2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var stats_ui: StatsUI = $StatsUI
 @onready var status_handler: StatusHandler = $StatusHandler
+@onready var modifier_handler: ModifierHandler = $ModifierHandler
 
 
 func _ready() -> void:
