77fd6ab6ff14db989dc7e504c443a80565211e46
Raajshreedhar <raaj.shreedhar@gmail.com>
2024-06-09 21:47:19 +0800

    Added Boss enemy to the game

---
+++ enemies/boss_willie/boss_willie_attack_action.gd
@@ -0,0 +1,37 @@
+extends EnemyAction
+
+@export var damage := 8
+
+
+func perform_action():
+	if not enemy or not target:
+		return
+	
+	var tween = create_tween().set_trans(Tween.TRANS_QUINT)
+	var start = enemy.global_position
+	var end = target.global_position + Vector2.RIGHT * 32
+	var damage_effect := DamageEffect.new()
+	var target_array: Array[Node] = [target]
+	damage_effect.amount = damage
+	damage_effect.sound = sound
+	
+	tween.tween_property(enemy, "global_position", end, 0.4)
+	tween.tween_callback(damage_effect.execute.bind(target_array))
+	tween.tween_interval(0.35)
+	tween.tween_callback(damage_effect.execute.bind(target_array))
+	tween.tween_interval(0.25)
+	tween.tween_property(enemy, "global_position", start, 0.4)
+	
+	tween.finished.connect(
+		func():
+			Events.enemy_action_completed.emit(enemy)
+	)
+
+
+func update_intent_text() -> void:
+	var player := target as Player
+	if not player:
+		return
+		
+	var modified_dmg := player.modifier_handler.get_modified_value(damage, Modifier.Type.DMG_TAKEN)
+	intent.current_text = intent.base_text % modified_dmg
